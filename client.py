4ede1537801e12ae099004303336688b97a87347
piccologatto01 <115500187+piccologatto01@users.noreply.github.com>
2024-04-12 13:25:33 +0300

    Добавлено часть Кобякова

--- client.py
+++ client.py
@@ -1,45 +1,53 @@
 import socket
-import struct
+import os
 
-HOST = (socket.gethostname(), 4444)
+from taskk import SOURCE_DIR
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(HOST)
-print("Client conected to", HOST)
-client_socket.send('1'.encode('utf-8'))
+folder1 = r"C:\Users\ASUS\Documents\testfolder1"
+folder2 = r"C:\Users\ASUS\Documents\testfolder2"  # Папка для синхронизации
 
-print("1. Добавить новую программу")
-print("2. Получить файл с выводом программы")
-
-choice = int(input("Выберите действие (1/2): "))
-packed_choice = struct.pack('i', choice)
-client_socket.send(packed_choice)
+HOST = '127.0.0.1'  # IP-адрес сервера
+PORT = 5000  # Порт сервера
 
 
-if choice == 1:
-    new_program_name = input("Введите имя новой программы: ")
-    pack_prog = struct.pack(f'I{len(new_program_name)}s', len(new_program_name), new_program_name.encode())
-    client_socket.send(pack_prog)
+def handle_client(conn):
+    '''
+    Данная функция учтанавливает сетевое взаимодействия
+    :param conn:
+    :return: Создает список изменений changes
+    Отправляет список изменений на сервер
+    '''
+    source_files = os.listdir(folder2)
 
-elif choice == 2:
-    program_name = input("Введите имя программы для получения файла с выводом: ")
-    packed_name = struct.pack(f'I{len(program_name)}s', len(program_name), program_name.encode())
-    client_socket.send(packed_name)
+    conn.sendall(str.encode(str(source_files)))
 
-    expected_size = 4096
-    data = client_socket.recv(4096)
-    received_size = struct.unpack(f'I', data[:struct.calcsize('I')])[0]
-    bytes_string = struct.unpack(f'I{received_size}s', data)[1]
+    changes = []
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        changes.extend(data.decode('utf-8').split(','))
 
-    while received_size > 0:
-        current_size = received_size if received_size < expected_size else expected_size
+    for change in changes:
+        if change.startswith('add'):
+            file = change[4:]
+            os.link(os.path.join(folder2, file), os.path.join(folder2, file))
+        elif change.startswith('del'):
+            file = change[4:]
+            os.unlink(os.path.join(folder2, file))
 
-        data_size = struct.unpack(f'I', data[:struct.calcsize('I')])[0]
-        bytes_string += struct.unpack(f'I{data_size}s', data)[1]
-        
-        received_size -= current_size
 
-    received_string = bytes_string.decode()
-    with open(f'Output_{program_name}.txt', 'w') as file:
-        file.write(received_string)
-client_socket.close()
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server.bind((HOST, PORT))
+
+    server.listen()
+
+    while True:
+        conn, addr = server.accept()
+        handle_client(conn)
+
+
+if __name__ == '__main__':
+    start_server()
